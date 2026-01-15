7f9acd015ff3f18aa0d7acaf6e9217a4b36448bf
hashaam-011 <hashaamkhan975@gmail.com>
2026-01-16 01:25:36 +0500

    changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,13 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./trainpi.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-else:
-    engine = create_engine(DATABASE_URL)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -23,4 +19,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
